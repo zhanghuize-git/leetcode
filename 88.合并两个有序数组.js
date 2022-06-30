2039c9adbb692a5f07defaba522b1f55ca01fe9c
zhanghuize <56755115+zhanghuize-git@users.noreply.github.com>
2022-06-30 14:43:01 +0800

    leetcode 88.合并两个有序数组

---
+++ 88.合并两个有序数组.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=javascript
+ *
+ * [88] 合并两个有序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  let i = m - 1,
+    j = n - 1,
+    k = m + n - 1;
+  while (i >= 0 || j >= 0) {
+    if (nums1[i] < nums2[j]) {
+      nums1[k--] = nums2[j--];
+    } else if (i < 0) {
+      nums1[k--] = nums2[j--];
+    } else {
+      nums1[k--] = nums1[i--];
+    }
+  }
+  return nums1;
+};
+// @lc code=end
+
